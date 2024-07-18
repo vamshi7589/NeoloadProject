ec08b4ce1f6a4287729c47d6d50249618955dcd5
e40035012 <e40035012@INM01054.rockwellcollins.com>
2024-07-18 12:28:49 +0530

    initial commit

---
+++ team/vus/@pet@store@users/%resources%/scripts/jsAction_4baabe1e-ee60-4b70-99dc-d20084cdd885.js
@@ -0,0 +1,12 @@
+﻿var file = new java.io.File("D:\\Workspace\\Neoload\\sample.txt");
+var writer = new java.io.FileWriter(file,true);
+
+var value1 = context.variableManager.getValue("jsessionid");
+
+var value2 = context.variableManager.getValue("LoginCheck");
+
+writer.write(value1+","+value2+"\n");
+//writer.write("Hello");
+
+writer.close();
+logger.debug("The values have been written to file");
